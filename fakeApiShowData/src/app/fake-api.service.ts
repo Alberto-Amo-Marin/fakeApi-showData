e806bbaf169213c29fea0e9f073bb2bcfcdd6fed
Alberto Amo Marin <albertoamo60@gmail.com>
2025-02-08 19:53:29 +0100

    added fake data via chance.js

--- fakeApiShowData/src/app/fake-api.service.ts
+++ fakeApiShowData/src/app/fake-api.service.ts
@@ -1,10 +1,15 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 
+const Chance = require('chance');
+const chance = new Chance();
+
 export interface Data {
   id: number;
   name: string;
   email: string;
+  address?: string;
+  phone?: string;
 }
 
 @Injectable({
@@ -14,14 +19,15 @@ export class FakeApiService {
 
   constructor() { }
 
-  // Genera 50 entradas de datos simulados
   getData(): Observable<Data[]> {
     const fakeData: Data[] = [];
-    for (let i = 1; i <= 50; i++) {
+    for (let i = 1; i <= 50; i++) {  // Generamos 50 registros
       fakeData.push({
         id: i,
-        name: `User ${i}`,
-        email: `user${i}@example.com`
+        name: chance.name(),  // Genera un nombre realista
+        email: chance.email(),  // Genera un correo electrónico realista
+        //address: `${faker.address.streetAddress()}, ${faker.address.city()}, ${faker.address.state()}`,  // Dirección más completa
+        //phone: faker.phone.phoneNumber(),  // Número de teléfono realista
       });
     }
     return of(fakeData);
